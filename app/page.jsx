f3be77c16b05e0cc054018959e56220207712bc3
Tania Banerjee <banerjeetania2019@gmail.com>
2023-10-30 22:10:23 +0530

    updating the ticket card

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,36 @@
 import TicketCard from "./{components}/TicketCard"
 
+const getTicket= async()=>{
+  try{
+    const res=await fetch("http://localhost:3000/api/Tickets", {
+      cache:"no-store"
+    })
+    return res.json();
+  }
+  catch(err){
+    console.log("Failed to get tickets", error);
+  }
+}
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const {tickets}= await getTicket();
+
+  const uniqueCategrories=[
+    ... new Set(tickets?.map(({category})=>category)),
+  ];
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard/>
-        <TicketCard/>
-        <TicketCard/>
-        <TicketCard/>
+      <div>
+        {tickets && uniqueCategrories?.map((unqiueCategory,categoryIndex)=><div key={categoryIndex} className="mb-4">
+          <h2>{unqiueCategory}</h2>
+          <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+            {tickets.filter((ticket)=>ticket.category==unqiueCategory).map((filteredTicket,_index)=>(
+              <TicketCard id={_index} key={_index} ticket={filteredTicket}/>
+            ))}
+          </div>
+
+        </div>)}
       </div> 
     </div>
   )
