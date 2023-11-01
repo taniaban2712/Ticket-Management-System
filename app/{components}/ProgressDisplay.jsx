0b636d68f0e8dd47af5cb2e0061b05991ee20eef
Tania Banerjee <banerjeetania2019@gmail.com>
2023-11-01 23:50:50 +0530

    adding dynamic cards

--- app/{components}/ProgressDisplay.jsx
+++ app/{components}/ProgressDisplay.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ProgressDisplay = () => {
+const ProgressDisplay = ({progress}) => {
   return (
     <div className='w-full bg-gray-400 rounded-full h-2.5'>
-        <div className='bg-blue-800 h-2.5 rounded-full' style={{width:"75%"}}>
+        <div className='bg-blue-800 h-2.5 rounded-full' style={{width:`${progress}%`}}>
 
         </div>
     </div>
--- app/{components}/TicketCard.jsx
+++ app/{components}/TicketCard.jsx
@@ -14,17 +14,17 @@ const TicketCard = ({ticket}) => {
             <DeleteBlock/>
             </div>
         </div>
-        <h4>Ticket Title</h4>
+        <h4>{ticket.title}</h4>
         <hr className='h-px border-0 bg-page mb-2'/>
-        <p className='whitespace-pre-wrap'>This is the ticket description</p>
+        <p className='whitespace-pre-wrap'>{ticket.description}</p>
         <div className='flex-grow'></div>
         <div className='flex mt-2'>
           <div className='flex flex-col'>
-            <p className='text-xs my-1'>12:11PM 28/10/23</p>
-            <ProgressDisplay/>
+            <p className='text-xs my-1'>{ticket.createdAt}</p>
+            <ProgressDisplay progress={ticket.progress}/>
           </div>
           <div className='ml-auto flex items-end'>
-            <StatusDisplay/>
+            <StatusDisplay status={ticket.status}/>
           </div>
         </div>
     </div>
